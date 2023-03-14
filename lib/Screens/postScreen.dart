153f58fa00fa12ac7392619e0831c2e5a1fad74d
unknown <fdcuf7392@gmail.com>
2023-03-14 20:13:44 +0500

    authentication/authorization

--- lib/Screens/postScreen.dart
+++ lib/Screens/postScreen.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import './loginScreen.dart';
+import '../utils/utilitites.dart';
 
 class PostScreen extends StatefulWidget {
   static const routeName = '/post-Screen';
@@ -11,8 +15,28 @@ class PostScreen extends StatefulWidget {
 class PostScreenState extends State<PostScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Post Screen')),
+    final auth = FirebaseAuth.instance;
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Center(
+              child: Text( 
+            'Post Screen',
+            style: TextStyle(color: Colors.white, fontSize: 40),
+          )),
+          IconButton(
+              onPressed: () {
+                auth.signOut().then((value) => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const loginScreen())).onError(
+                    (error, stackTrace) =>
+                        utilities().showMsg(error.toString())));
+              },
+              icon: const Icon(Icons.logout_rounded))
+        ],
+      ),
     );
   }
 }
